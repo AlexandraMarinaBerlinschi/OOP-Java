cb5589bf381e30f8901b5dfb0edd2e17f58b54a7
AlexandraMarinaBerlinschi <alexandra-marina.berlinschi@s.unibuc.ro>
2024-03-20 10:52:08 +0200

    update tema

--- lab4-tema/src/Application.java
+++ lab4-tema/src/Application.java
@@ -7,7 +7,14 @@ public class Application {
         ShoppingMall shoppingMall = new ShoppingMall(new DebitCard("Popescu", 500));
         shoppingMall.achizitie(200);
 
+        ShoppingMall shoppingMall1 = new ShoppingMall(new DebitCard("Marinescu", 500));
+        shoppingMall1.achizitie(300);
+
+        ShoppingMall shoppingMall2 = new ShoppingMall(new DebitCard("Popa", 500));
+        shoppingMall2.achizitie(700);
+
         shoppingMall = new ShoppingMall(new CreditCard("Popovici"));
         shoppingMall.achizitie(300);
+
     }
 }
